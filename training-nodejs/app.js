6cd31d44002e908ed3cc26ee704b73c328067aa1
TaiTD <tai.td@citigo.net>
2019-04-23 11:54:31 +0700

    no message

--- training-nodejs/app.js
+++ training-nodejs/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const moduleConfig = require('./mmodule/config');
 
 http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello NodeJS2</h1>');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello NodeJS2</h1>');
 }).listen(moduleConfig.port);
